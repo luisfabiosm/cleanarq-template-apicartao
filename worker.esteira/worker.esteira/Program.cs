5861be036a81404ff8aa628020193adbcbc107d9
@w3clouddev <cloud-dev@w3as.com.br>
2023-07-13 08:55:59 -0300

    ajustes apos novos testes

---
+++ worker.esteira/worker.esteira/Program.cs
@@ -0,0 +1,10 @@
+using worker.esteira;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
